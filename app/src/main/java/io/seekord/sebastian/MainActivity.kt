d6d1ce3adc259c0960ef514d8b17e87c3cdb08d4
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2017-11-23 00:52:36 +0300

    Add toolbar

--- app/src/main/java/io/seekord/sebastian/MainActivity.kt
+++ app/src/main/java/io/seekord/sebastian/MainActivity.kt
@@ -1,12 +1,17 @@
 package io.seekord.sebastian
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.title = "Dummy title"
+        toolbar.subtitle = "Dummy subtitle"
     }
+
 }
