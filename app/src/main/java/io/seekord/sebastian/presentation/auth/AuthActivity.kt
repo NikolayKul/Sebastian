e2f6f126ef4952e37bc4a9fc24a2215b1f698534
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2017-12-04 12:31:24 +0300

    Auth: add data binding

--- app/src/main/java/io/seekord/sebastian/presentation/auth/AuthActivity.kt
+++ app/src/main/java/io/seekord/sebastian/presentation/auth/AuthActivity.kt
@@ -1,15 +1,20 @@
 package io.seekord.sebastian.presentation.auth
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import io.seekord.sebastian.R
+import io.seekord.sebastian.databinding.ActivityAuthBinding
 import io.seekord.sebastian.presentation.base.BaseActivity
 
 /**
  * Created by Nikolay Kulachenko
  */
 class AuthActivity : BaseActivity() {
+    private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_auth)
     }
+
 }
--- app/src/main/java/io/seekord/sebastian/presentation/main/MainActivity.kt
+++ app/src/main/java/io/seekord/sebastian/presentation/main/MainActivity.kt
@@ -1,15 +1,18 @@
 package io.seekord.sebastian.presentation.main
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import io.seekord.sebastian.R
+import io.seekord.sebastian.databinding.ActivityMainBinding
 import io.seekord.sebastian.presentation.base.BaseActivity
 
 class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         toolbar.title = "Dummy title"
         toolbar.subtitle = "Dummy subtitle"
