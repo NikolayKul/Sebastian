5f98a373ab6fba1cd25798973015ec5253db1ee6
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2017-12-12 01:59:41 +0300

    Auth: set all view-commands' strategy: `OneExecutionStateStrategy`

--- app/src/main/java/io/seekord/sebastian/presentation/auth/AuthContract.kt
+++ app/src/main/java/io/seekord/sebastian/presentation/auth/AuthContract.kt
@@ -1,11 +1,14 @@
 package io.seekord.sebastian.presentation.auth
 
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import io.seekord.sebastian.presentation.base.LoadingMvpView
 
 /**
  * Created by nikolay
  */
 
+@StateStrategyType(OneExecutionStateStrategy::class)
 interface AuthMvpView : LoadingMvpView {
     fun showLoginSuccess()
 }
--- app/src/main/java/io/seekord/sebastian/presentation/base/BaseMvpViews.kt
+++ app/src/main/java/io/seekord/sebastian/presentation/base/BaseMvpViews.kt
@@ -1,15 +1,19 @@
 package io.seekord.sebastian.presentation.base
 
 import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 
 /**
  * Created by nikolay
  */
 
+@StateStrategyType(OneExecutionStateStrategy::class)
 interface ErrorMvpView : MvpView {
     fun showError(error: Throwable)
 }
 
+@StateStrategyType(OneExecutionStateStrategy::class)
 interface LoadingMvpView : ErrorMvpView {
     fun showLoading()
     fun hideLoading()
