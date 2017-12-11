cce9aecf8d0e3064216aa55faf284de87995eb3a
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2017-12-12 01:00:30 +0300

    Auth: attach Disposables to lifecycle

--- app/src/main/java/io/seekord/sebastian/presentation/auth/AuthPresenter.kt
+++ app/src/main/java/io/seekord/sebastian/presentation/auth/AuthPresenter.kt
@@ -1,6 +1,7 @@
 package io.seekord.sebastian.presentation.auth
 
 import com.arellomobile.mvp.InjectViewState
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.seekord.sebastian.domain.auth.AuthUseCase
 import io.seekord.sebastian.domain.auth.models.AuthCredentials
 import io.seekord.sebastian.presentation.base.BasePresenter
@@ -16,12 +17,14 @@ class AuthPresenter @Inject constructor(private val authUseCase: AuthUseCase)
     : BasePresenter<AuthMvpView>() {
 
     fun auth(authCredentials: AuthCredentials) {
-        viewState.showLoading()
         authUseCase.auth(authCredentials)
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { viewState.showLoading() }
                 .doOnTerminate { viewState.hideLoading() }
                 .subscribe(
                         { Timber.d("Its ok") },
                         { viewState.showError(it) })
+                .attachToLifecycle()
     }
 
 }
--- app/src/main/java/io/seekord/sebastian/presentation/base/BasePresenter.kt
+++ app/src/main/java/io/seekord/sebastian/presentation/base/BasePresenter.kt
@@ -2,9 +2,21 @@ package io.seekord.sebastian.presentation.base
 
 import com.arellomobile.mvp.MvpPresenter
 import com.arellomobile.mvp.MvpView
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 /**
  * Created by nikolay
  */
 
-abstract class BasePresenter<T : MvpView> : MvpPresenter<T>()
+abstract class BasePresenter<T : MvpView> : MvpPresenter<T>() {
+    private val disposables: CompositeDisposable = CompositeDisposable()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposables.clear()
+    }
+
+    protected fun Disposable.attachToLifecycle() = disposables.add(this)
+
+}
