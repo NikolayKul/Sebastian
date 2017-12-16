86b5e0cc7d2e5be0601a0edba17f1816ff0c649a
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2017-12-16 21:39:44 +0100

    Auth: replace TODOs with stubs

--- app/src/main/java/io/seekord/sebastian/data/repository/auth/AuthRepository.kt
+++ app/src/main/java/io/seekord/sebastian/data/repository/auth/AuthRepository.kt
@@ -1,23 +1,31 @@
 package io.seekord.sebastian.data.repository.auth
 
 import dagger.Reusable
+
 import io.reactivex.Completable
 import io.reactivex.Single
 import io.seekord.sebastian.domain.auth.AuthData
 import io.seekord.sebastian.domain.auth.AuthParams
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 /**
  * Created by nikolay
  */
 
+private const val DEFAULT_DELAY = 2_000L
+
 @Reusable
 class AuthRepository @Inject constructor() {
+    private val authData = AuthData("requestToken", "refreshToken")
 
-    fun auth(authParams: AuthParams): Single<AuthData> = TODO("Not implemented yet")
+    fun auth(authParams: AuthParams) = Single.just(authData)
+            .delay(DEFAULT_DELAY, TimeUnit.MILLISECONDS)
 
-    fun getAuthData(): Single<AuthData> = TODO("Not implemented yet")
+    fun getAuthData() = Single.just(authData)
+            .delay(DEFAULT_DELAY, TimeUnit.MILLISECONDS)
 
-    fun saveAuthData(data: AuthData): Completable = TODO("Not implemented yet")
+    fun saveAuthData(data: AuthData) = Completable.complete()
+            .delay(DEFAULT_DELAY, TimeUnit.MILLISECONDS)
 
 }
