b2da78400289ec6b652792c295c8312448ca0c49
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2017-12-22 21:57:47 +0300

    Coroutines: switch `presentation` layer to the coroutines

--- app/src/main/java/io/seekord/sebastian/presentation/auth/AuthPresenter.kt
+++ app/src/main/java/io/seekord/sebastian/presentation/auth/AuthPresenter.kt
@@ -1,12 +1,14 @@
 package io.seekord.sebastian.presentation.auth
 
 import com.arellomobile.mvp.InjectViewState
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.seekord.sebastian.domain.auth.AuthParams
 import io.seekord.sebastian.domain.auth.AuthUseCase
 import io.seekord.sebastian.presentation.base.BasePresenter
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
 import javax.inject.Inject
-
 
 /**
  * Created by nikolay
@@ -16,17 +18,26 @@ import javax.inject.Inject
 class AuthPresenter @Inject constructor(
         private val authUseCase: AuthUseCase
 ) : BasePresenter<AuthMvpView>() {
+    private var job: Job? = null
 
     fun auth(username: String, password: String) {
-        val authParams = AuthParams(username, password)
-        authUseCase.execute(authParams)
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe { viewState.showLoading() }
-                .doOnTerminate { viewState.hideLoading() }
-                .subscribe(
-                        { viewState.showLoginSuccess() },
-                        { viewState.showError(it) })
-                .attachToLifecycle()
+        job = doAuth(AuthParams(username, password))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job?.cancel()
+    }
+
+    private fun doAuth(authParams: AuthParams) = launch(UI) {
+        viewState.showLoading()
+        try {
+            async { authUseCase.execute(authParams) }.await()
+            viewState.showLoginSuccess()
+        } catch (error: Exception) {
+            viewState.showError(error)
+        }
+        viewState.hideLoading()
     }
 
 }
--- app/src/main/java/io/seekord/sebastian/presentation/main/MainPresenter.kt
+++ app/src/main/java/io/seekord/sebastian/presentation/main/MainPresenter.kt
@@ -1,9 +1,12 @@
 package io.seekord.sebastian.presentation.main
 
 import com.arellomobile.mvp.InjectViewState
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.seekord.sebastian.domain.auth.GetAuthTokenUseCase
 import io.seekord.sebastian.presentation.base.BasePresenter
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -15,22 +18,26 @@ import javax.inject.Inject
 class MainPresenter @Inject constructor(
         private val getAuthTokenUseCase: GetAuthTokenUseCase
 ) : BasePresenter<MainMvpView>() {
+    private var job: Job? = null
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
-        showCurrentAuthToken()
+        job = doShowCurrentAuthToken()
     }
 
-    private fun showCurrentAuthToken() {
-        getAuthTokenUseCase.execute()
-                .map { it.accessToken }
-                .doOnError { Timber.e(it) }
-                .onErrorReturn { "No auth token" }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        { viewState.showCurrentAuthToken(it) },
-                        { Timber.e(it) })
-                .attachToLifecycle()
+    override fun onDestroy() {
+        super.onDestroy()
+        job?.cancel()
+    }
+
+    private fun doShowCurrentAuthToken() = async {
+        val token = try {
+            getAuthTokenUseCase.execute().accessToken
+        } catch (e: Exception) {
+            Timber.e(e)
+            "No auth token"
+        }
+        launch(UI) { viewState.showCurrentAuthToken(token) }
     }
 
 }
