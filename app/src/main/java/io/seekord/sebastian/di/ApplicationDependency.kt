316c9108b5071ef1e0c67565044b795e8f0354c7
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-04-08 12:23:14 +0300

    Build api via Dagger instead of a factory

--- app/src/main/java/io/seekord/sebastian/di/ApplicationDependency.kt
+++ app/src/main/java/io/seekord/sebastian/di/ApplicationDependency.kt
@@ -2,13 +2,24 @@ package io.seekord.sebastian.di
 
 import android.app.Application
 import android.content.Context
+import com.tickaroo.tikxml.TikXml
+import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
 import dagger.Component
 import dagger.Module
 import dagger.Provides
-import io.seekord.sebastian.data.network.NetworkApiFactory
+import dagger.multibindings.IntoSet
+import io.seekord.sebastian.BuildConfig
+import io.seekord.sebastian.data.network.RssApi
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import ru.terrakok.cicerone.Cicerone
 import ru.terrakok.cicerone.NavigatorHolder
 import ru.terrakok.cicerone.Router
+import timber.log.Timber
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
@@ -29,7 +40,7 @@ class ApplicationModule(private val app: Application) {
     @Provides
     @Singleton
     @AppContext
-    fun provideApplicationContext(): Context = app.applicationContext
+    fun applicationContext(): Context = app.applicationContext
 
 }
 
@@ -40,21 +51,66 @@ class RouterModule {
 
     @Provides
     @Singleton
-    fun provideNavigatorHolder(): NavigatorHolder = cicerone.navigatorHolder
+    fun navigatorHolder(): NavigatorHolder = cicerone.navigatorHolder
 
     @Provides
     @Singleton
-    fun provideRouter(): Router = cicerone.router
+    fun router(): Router = cicerone.router
 
 }
 
+
+private const val GEEKTIMES_URL = "https://geektimes.ru/rss/"
 
 @Module
 class NetworkModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(factory: NetworkApiFactory) = factory.create()
+    fun rssApi(retrofit: Retrofit): RssApi = retrofit.create(RssApi::class.java)
+
+    @Provides
+    fun retrofit(client: OkHttpClient,
+                 converterFactories: Set<@JvmSuppressWildcards Converter.Factory>): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl(GEEKTIMES_URL)
+                .client(client)
+                .apply { converterFactories().addAll(converterFactories) }
+                .build()
+    }
+
+    @Provides
+    fun client(interceptors: Set<@JvmSuppressWildcards Interceptor>): OkHttpClient = OkHttpClient.Builder()
+            .apply { interceptors().addAll(interceptors) }
+            .build()
+
+    // converters
+
+    @Provides
+    @IntoSet
+    fun converterFactoryScalars(): Converter.Factory = ScalarsConverterFactory.create()
+
+    @Provides
+    @IntoSet
+    fun converterFactoryXml(): Converter.Factory = TikXml.Builder()
+            .exceptionOnUnreadXml(false)
+            .build()
+            .let { TikXmlConverterFactory.create(it) }
+
+    // interceptors
+
+    @Provides
+    @IntoSet
+    fun interceptorLogging(): Interceptor {
+        val level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+
+        return HttpLoggingInterceptor { Timber.tag("[OkHttp]").d(it) }
+                .setLevel(level)
+    }
 
 }
 
