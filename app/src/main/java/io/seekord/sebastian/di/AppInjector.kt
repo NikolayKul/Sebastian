8155492b4f511a401d6be6d1f0a02a8ff8df5d6c
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-04-08 23:12:39 +0300

    Inject fragments as well

--- app/src/main/java/io/seekord/sebastian/di/AppInjector.kt
+++ app/src/main/java/io/seekord/sebastian/di/AppInjector.kt
@@ -2,8 +2,13 @@ package io.seekord.sebastian.di
 
 import android.app.Activity
 import android.app.Application
+import android.content.Context
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import android.support.v4.app.FragmentManager
 import dagger.android.AndroidInjection
+import dagger.android.support.AndroidSupportInjection
 import io.seekord.sebastian.App
 import io.seekord.sebastian.di.application.DaggerAppComponent
 
@@ -17,12 +22,19 @@ object AppInjector {
 
     fun init(app: App) {
         injectApp(app)
-        app.registerOnActivityCreateCallback { activity, _ -> injectActivity(activity) }
+        app.registerOnActivityCreateCallback(this::handleActivity)
     }
 
-    private fun injectActivity(activity: Activity) {
+    private fun handleActivity(activity: Activity) {
         if (activity is Injectable) {
             AndroidInjection.inject(activity)
+        }
+        (activity as? FragmentActivity)?.registerOnFragmentAttachedCallback(this::handleFragment)
+    }
+
+    private fun handleFragment(fragment: Fragment) {
+        if (fragment is Injectable) {
+            AndroidSupportInjection.inject(fragment)
         }
     }
 
@@ -37,15 +49,28 @@ object AppInjector {
 
 
 /**
+ * Helper method that uses [FragmentManager.registerFragmentLifecycleCallbacks]
+ * to invoke [block] on *onFragmentAttached*
+ */
+private fun FragmentActivity.registerOnFragmentAttachedCallback(block: (Fragment) -> Unit) {
+    supportFragmentManager.registerFragmentLifecycleCallbacks(object : FragmentManager.FragmentLifecycleCallbacks() {
+        override fun onFragmentAttached(fm: FragmentManager, fragment: Fragment, context: Context) {
+            block(fragment)
+        }
+    }, true)
+}
+
+
+/**
  * Helper method that uses [Application.registerActivityLifecycleCallbacks]
  * to invoke [block] on *onActivityCreated*
  *
  * @author NikolayKul
  */
-private fun Application.registerOnActivityCreateCallback(block: (Activity, Bundle?) -> Unit) {
+private fun Application.registerOnActivityCreateCallback(block: (Activity) -> Unit) {
     registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
         override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-            block(activity, savedInstanceState)
+            block(activity)
         }
 
         override fun onActivityStarted(activity: Activity) {
