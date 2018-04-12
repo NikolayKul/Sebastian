62aa511a56f9d13fccd9421f97e0160ebd530e05
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-04-12 18:16:31 +0300

    Fetch date

--- app/src/main/java/io/seekord/sebastian/data/models/RssPreviewDto.kt
+++ app/src/main/java/io/seekord/sebastian/data/models/RssPreviewDto.kt
@@ -2,6 +2,7 @@ package io.seekord.sebastian.data.models
 
 import com.tickaroo.tikxml.annotation.PropertyElement
 import com.tickaroo.tikxml.annotation.Xml
+import org.joda.time.DateTime
 
 /**
  * @author NikolayKul
@@ -13,14 +14,17 @@ class RssPreviewDto {
     @PropertyElement
     lateinit var title: String
 
-    @PropertyElement(name = "guid")
-    lateinit var link: String
+    @PropertyElement(name = "pubDate")
+    lateinit var date: DateTime
 
     @PropertyElement
     lateinit var description: String
 
+    @PropertyElement(name = "guid")
+    lateinit var link: String
+
     override fun toString(): String {
-        return "RssPreviewDto(title='$title', link='$link', description='$description')"
+        return "RssPreviewDto(title='$title', date=$date, description='$description', link='$link')"
     }
 
 }
--- app/src/main/java/io/seekord/sebastian/utils/network/TikXmlFactory.kt
+++ app/src/main/java/io/seekord/sebastian/utils/network/TikXmlFactory.kt
@@ -1,6 +1,9 @@
 package io.seekord.sebastian.utils.network
 
 import com.tickaroo.tikxml.TikXml
+import com.tickaroo.tikxml.TypeConverter
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
 /**
  * Xml factory for [TikXml]
@@ -11,8 +14,24 @@ object TikXmlFactory {
 
     fun create(): TikXml = TikXml.Builder()
             .exceptionOnUnreadXml(false)
+            .addTypeConverter(DateTime::class.java, DateTimeTypeConverter())
             .build()
 
 }
 
 
+/**
+ * [TypeConverter] for [DateTime]
+ *
+ * @author NikolayKul
+ */
+private class DateTimeTypeConverter : TypeConverter<DateTime> {
+    private val formatterRfc1123 = DateTimeFormat
+            .forPattern("EEE, dd MMM yyyy HH:mm:ss 'GMT'")
+            .withZoneUTC()
+
+    override fun write(value: DateTime): String = formatterRfc1123.print(value)
+
+    override fun read(value: String): DateTime = formatterRfc1123.parseDateTime(value)
+
+}
