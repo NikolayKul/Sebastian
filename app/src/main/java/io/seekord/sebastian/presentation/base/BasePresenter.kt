0d41a938821d917626fb976011b9ca2ef418653c
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-04-15 15:42:35 +0300

    Clear `Jobs` on destroy

--- app/src/main/java/io/seekord/sebastian/presentation/base/BasePresenter.kt
+++ app/src/main/java/io/seekord/sebastian/presentation/base/BasePresenter.kt
@@ -4,6 +4,7 @@ import com.arellomobile.mvp.MvpPresenter
 import com.arellomobile.mvp.MvpView
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import kotlinx.coroutines.experimental.Job
 
 /**
  * @author NikolayKul
@@ -11,12 +12,24 @@ import io.reactivex.disposables.Disposable
 
 abstract class BasePresenter<T : MvpView> : MvpPresenter<T>() {
     private val disposables: CompositeDisposable = CompositeDisposable()
+    private val jobs: MutableSet<Job> = mutableSetOf()
 
     override fun onDestroy() {
         super.onDestroy()
         disposables.clear()
+        clearJobs()
     }
 
     protected fun Disposable.attachToLifecycle() = disposables.add(this)
+
+    protected fun Job.attachToLifecycle() {
+        jobs += this
+        invokeOnCompletion { jobs.remove(this) }
+    }
+
+    private fun clearJobs() {
+        jobs.filter { it.isActive }
+                .forEach { it.cancel() }
+    }
 
 }
--- app/src/main/java/io/seekord/sebastian/presentation/main/MainPresenter.kt
+++ app/src/main/java/io/seekord/sebastian/presentation/main/MainPresenter.kt
@@ -5,7 +5,6 @@ import io.seekord.sebastian.domain.rss.GetRssItemsUseCase
 import io.seekord.sebastian.presentation.base.BasePresenter
 import io.seekord.sebastian.utils.coroutine.CoroutineContextProvider.IO
 import io.seekord.sebastian.utils.coroutine.CoroutineContextProvider.UI
-import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.launch
 import timber.log.Timber
 import javax.inject.Inject
@@ -18,25 +17,20 @@ import javax.inject.Inject
 class MainPresenter @Inject constructor(
         private val itemsUseCase: GetRssItemsUseCase
 ) : BasePresenter<MainMvpView>() {
-    private var job: Job? = null
-
-    override fun onDestroy() {
-        super.onDestroy()
-        job?.cancel()
-    }
 
     fun loadRssPreviews() {
-        job = launch(IO) {
+        launch(IO) {
             val items = try {
                 itemsUseCase.getPssItems()
             } catch (e: Exception) {
-                Timber.e(e)
+                viewState.showError(e)
                 return@launch
             }
 
             Timber.d("Items: $items")
             launch(UI) { viewState.showRssPreviews(items) }
-        }
+
+        }.attachToLifecycle()
     }
 
 }
