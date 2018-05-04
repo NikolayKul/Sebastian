f202099134d36ca11899e082599094a9801c9465
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-05-04 18:09:50 +0300

    Create a UseCase test frame

---
+++ app/src/test/java/io/seekord/sebastian/domain/channel/GetChannelUseCaseTest.kt
@@ -0,0 +1,41 @@
+package io.seekord.sebastian.domain.channel
+
+import io.seekord.sebastian.data.repository.RssRepository
+import io.seekord.sebastian.domain.channel.models.RssChannel
+import io.seekord.sebastian.utils.mockito.givenSuspended
+import io.seekord.sebastian.utils.mockito.willReturn
+import kotlinx.coroutines.experimental.runBlocking
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class GetChannelUseCaseTest {
+    @Mock private lateinit var repository: RssRepository
+    private lateinit var useCase: GetChannelUseCase
+
+    @Before
+    fun setUp() {
+        useCase = GetChannelUseCase(repository)
+    }
+
+    @Test
+    fun `load channel`() = runBlocking {
+        val givenChannel = RssChannel("title", "link", "description")
+        givenSuspended { repository.getRssChannel() } willReturn { givenChannel }
+
+        val resultChannel = useCase.execute()
+
+        assertThat(resultChannel, `is`(givenChannel))
+    }
+
+    @Test
+    fun `throw no internet exception`() {
+        TODO("Create NetworkManager")
+    }
+
+}
