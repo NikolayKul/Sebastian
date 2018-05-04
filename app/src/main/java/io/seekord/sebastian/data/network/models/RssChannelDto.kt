2fecfebfc14f77d88d780b17b06b7d3195d67d61
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-05-04 22:47:38 +0300

    Add path

--- app/src/main/java/io/seekord/sebastian/data/network/models/RssChannelDto.kt
+++ app/src/main/java/io/seekord/sebastian/data/network/models/RssChannelDto.kt
@@ -1,11 +1,22 @@
 package io.seekord.sebastian.data.network.models
 
+import com.tickaroo.tikxml.annotation.Path
+import com.tickaroo.tikxml.annotation.PropertyElement
 import com.tickaroo.tikxml.annotation.Xml
 
 @Xml(name = "rss")
 class RssChannelDto(
+
+        @Path("channel")
+        @PropertyElement
         var title: String? = null,
+
+        @Path("channel")
+        @PropertyElement
         var link: String? = null,
+
+        @Path("channel")
+        @PropertyElement
         var description: String? = null
 ) {
 
