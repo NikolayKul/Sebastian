1e52dd38756c24983af0a11b727872f614fde94d
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-05-10 23:37:39 +0300

    Create a MVVM frame

--- app/src/main/java/com/nikolaykul/sebastian/presentation/main/MainActivity.kt
+++ app/src/main/java/com/nikolaykul/sebastian/presentation/main/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.nikolaykul.sebastian.presentation.main
 
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
 import android.support.v7.widget.LinearLayoutManager
-import com.arellomobile.mvp.presenter.InjectPresenter
-import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.nikolaykul.sebastian.R
 import com.nikolaykul.sebastian.databinding.ActivityMainBinding
 import com.nikolaykul.sebastian.domain.rss.models.RssFeed
 import com.nikolaykul.sebastian.presentation.base.BaseActivity
 import com.nikolaykul.sebastian.presentation.main.adapter.MainFeedAdapter
+import com.nikolaykul.sebastian.utils.common.ViewModelFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import timber.log.Timber
 import javax.inject.Inject
+import kotlin.reflect.KProperty
 
 /**
  * @author NikolayKul
@@ -22,16 +27,29 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), MainMvpView {
         fun createStartIntent(context: Context) = Intent(context, MainActivity::class.java)
     }
 
-    @Inject @InjectPresenter lateinit var presenter: MainPresenter
+    @Inject lateinit var viewModelFactory: ViewModelFactory
+    private lateinit var viewModel: MainViewModel //by viewModelD(this, viewModelFactory)
     private val adapter = MainFeedAdapter()
-
-    @ProvidePresenter
-    fun providePresenter() = presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initRecyclerView()
         initListeners()
+        viewModel = ViewModelProviders.of(this, viewModelFactory)[MainViewModel::class.java]
+        initObservers()
+    }
+
+    private fun initObservers() {
+        viewModel.observeFeeds()
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        (this::showFeeds),
+                        (Timber::e))
+        viewModel.observeLoading()
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { Timber.d("Show loading: %b", it) },
+                        (Timber::e))
     }
 
     override fun getLayoutId() = R.layout.activity_main
@@ -41,7 +59,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), MainMvpView {
     }
 
     private fun initListeners() {
-        binding.fab.setOnClickListener { presenter.loadFeeds() }
+        binding.fab.setOnClickListener { viewModel.loadChannel() }
+//        binding.fab.setOnClickListener { presenter.loadFeeds() }
     }
 
     private fun initRecyclerView() {
@@ -50,5 +69,32 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), MainMvpView {
             it.adapter = adapter
         }
     }
+
+}
+
+private inline fun <reified T : ViewModel> viewModelD(
+        activity: FragmentActivity,
+        viewModelFactory: ViewModelFactory): ViewModelDelegate<T> {
+    return ViewModelDelegate(T::class.java, activity, viewModelFactory)
+}
+
+private class ViewModelDelegate<T : ViewModel>(
+        private val clazz: Class<T>,
+        private val activity: FragmentActivity,
+        private val viewModelFactory: ViewModelFactory
+) {
+
+    private var viewModel: T? = null
+
+    operator fun getValue(thisRef: Any?, prop: KProperty<*>): T {
+        return viewModel ?: getAndCache()
+    }
+
+    operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: T) {
+        viewModel = value
+    }
+
+    private fun getAndCache() = ViewModelProviders.of(activity, viewModelFactory)[clazz]
+            .also { viewModel = it }
 
 }
--- app/src/main/java/com/nikolaykul/sebastian/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/nikolaykul/sebastian/presentation/main/MainViewModel.kt
@@ -1,8 +1,47 @@
 package com.nikolaykul.sebastian.presentation.main
 
 import android.arch.lifecycle.ViewModel
+import com.nikolaykul.sebastian.domain.rss.GetChannelUseCase
+import com.nikolaykul.sebastian.domain.rss.models.RssFeed
+import com.nikolaykul.sebastian.utils.common.CoroutineContextProvider.UI
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.subjects.BehaviorSubject
+import kotlinx.coroutines.experimental.launch
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(
+        private val getChannelUseCase: GetChannelUseCase
+) : ViewModel() {
+
+    private val loadingRelay = BehaviorRelay<Boolean>()
+    private val feedsRelay = BehaviorRelay<List<RssFeed>>()
+
+    fun observeLoading() = loadingRelay.observe()
+
+    fun observeFeeds() = feedsRelay.observe()
+
+    fun loadChannel() {
+        launch(UI) {
+            loadingRelay.push(true)
+            val channel = getChannelUseCase.execute()
+            loadingRelay.push(false)
+            feedsRelay.push(channel.feeds)
+        }
+    }
+
+}
+
+// TODO: move to utils
+private class BehaviorRelay<T>(
+        private val _relay: BehaviorSubject<T> = BehaviorSubject.create()
+) {
+
+    fun observe(strategy: BackpressureStrategy = BackpressureStrategy.LATEST): Flowable<T> =
+            _relay.toFlowable(strategy)
+
+    fun push(value: T) {
+        _relay.onNext(value)
+    }
 
 }
