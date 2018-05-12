b88502a95f9ad2d6bae97567a2626d5aaab9302e
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-05-12 16:58:25 +0300

    Update formatting

--- app/src/test/java/com/nikolaykul/sebastian/utils/rules/CoroutineContextRule.kt
+++ app/src/test/java/com/nikolaykul/sebastian/utils/rules/CoroutineContextRule.kt
@@ -1,24 +1,24 @@
 package com.nikolaykul.sebastian.utils.rules
 
 import com.nikolaykul.sebastian.utils.common.CoroutineContextProvider
+import kotlinx.coroutines.experimental.Unconfined
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
 
 /**
- * @author NikolayKul
+ * A [TestRule] that mocks all [CoroutineContextProvider]'s contexts with the [Unconfined] one
+ *
+ * @see [CoroutineContextProvider.mockContexts]
  */
-
 class CoroutineContextRule : TestRule {
-
-    override fun apply(base: Statement?, description: Description?): Statement {
+    override fun apply(base: Statement, description: Description?): Statement {
         return object : Statement() {
             @Throws(Throwable::class)
             override fun evaluate() {
                 CoroutineContextProvider.mockContexts()
-                base?.evaluate()
+                base.evaluate()
             }
         }
     }
-
 }
