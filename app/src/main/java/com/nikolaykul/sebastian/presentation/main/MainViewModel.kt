a50247e4b15b2da5e0c24486e59825d4a099638c
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-05-12 17:09:25 +0300

    Attach jobs to the lifecycle

--- app/src/main/java/com/nikolaykul/sebastian/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/nikolaykul/sebastian/presentation/main/MainViewModel.kt
@@ -1,8 +1,8 @@
 package com.nikolaykul.sebastian.presentation.main
 
-import android.arch.lifecycle.ViewModel
 import com.nikolaykul.sebastian.domain.rss.GetChannelUseCase
 import com.nikolaykul.sebastian.domain.rss.models.RssFeed
+import com.nikolaykul.sebastian.presentation.base.BaseViewModel
 import com.nikolaykul.sebastian.utils.common.CoroutineContextProvider.UI
 import com.nikolaykul.sebastian.utils.rx.RxRelay
 import kotlinx.coroutines.experimental.launch
@@ -10,7 +10,7 @@ import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
         private val getChannelUseCase: GetChannelUseCase
-) : ViewModel() {
+) : BaseViewModel() {
 
     private val loadingRelay = RxRelay<Boolean>()
     private val feedsRelay = RxRelay<List<RssFeed>>()
@@ -25,7 +25,7 @@ class MainViewModel @Inject constructor(
             val channel = getChannelUseCase.execute()
             feedsRelay.push(channel.feeds)
             loadingRelay.push(false)
-        }
+        }.attachToLifecycle()
     }
 
 }
