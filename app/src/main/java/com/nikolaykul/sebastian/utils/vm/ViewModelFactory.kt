0868beefafbc25ecf131fedd0bceebb9d34bf790
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-05-30 12:47:33 +0300

    Don't lookup the children

--- app/src/main/java/com/nikolaykul/sebastian/utils/vm/ViewModelFactory.kt
+++ app/src/main/java/com/nikolaykul/sebastian/utils/vm/ViewModelFactory.kt
@@ -5,35 +5,19 @@ import android.arch.lifecycle.ViewModelProvider
 import javax.inject.Inject
 import javax.inject.Provider
 
-
-/**
- * Alias for [Map.Entry.key] used by [ViewModelFactory.providers]
- */
-typealias ProviderKey = Class<out ViewModel>
-
-
-/**
- * Alias for [Map.Entry.value] used by [ViewModelFactory.providers]
- */
-typealias ProviderValue = @JvmSuppressWildcards Provider<ViewModel>
-
-
 class ViewModelFactory @Inject constructor(
-        private val providers: Map<ProviderKey, ProviderValue>
+    private val providers: Map<ProviderKey, ProviderValue>
 ) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(clazz: Class<T>): T {
-        val provider = providers[clazz] ?: findProviderOrThrow(clazz)
+        val provider = providers[clazz]
+                ?: throw IllegalArgumentException("Unknown ViewModel class: $clazz")
         return provider.get() as T
     }
 
-    // can't reify T because it's not known at the compile time. So fall back to the Class<T>
-    private fun <T : ViewModel> findProviderOrThrow(clazz: Class<T>): Provider<ViewModel> {
-        val provider = providers.entries
-                .firstOrNull { clazz.isAssignableFrom(it.key) }
-                ?.value
-        return provider ?: throw IllegalArgumentException("Unknown ViewModel class $clazz")
-    }
-
 }
+
+
+typealias ProviderKey = Class<out ViewModel>
+typealias ProviderValue = @JvmSuppressWildcards Provider<ViewModel>
