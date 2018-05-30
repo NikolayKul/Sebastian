bfcb5f3afc562556935b60e6104372e3fd10f39c
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-05-30 12:44:01 +0300

    Configure scopes

--- app/src/main/java/com/nikolaykul/sebastian/di/application/AppComponent.kt
+++ app/src/main/java/com/nikolaykul/sebastian/di/application/AppComponent.kt
@@ -5,7 +5,9 @@ import com.nikolaykul.sebastian.App
 import com.nikolaykul.sebastian.di.activity.ActivityInjectionModule
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(
     modules = [
         ActivityInjectionModule::class,
