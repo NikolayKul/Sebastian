c85b0c142d6b32d375a460b808dc313cf4c1d849
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-12-16 14:06:34 +0300

    Explicitly specify return type

--- app/src/main/java/com/nikolaykul/sebastian/domain/rss/GetChannelUseCase.kt
+++ app/src/main/java/com/nikolaykul/sebastian/domain/rss/GetChannelUseCase.kt
@@ -8,9 +8,7 @@ import javax.inject.Inject
 
 @Reusable
 class GetChannelUseCase @Inject constructor(
-        private val repository: RssRepository
+    private val repository: RssRepository
 ) : UseCaseNoParams<RssChannel> {
-
-    override suspend fun execute() = repository.getRssChannel()
-
+    override suspend fun execute(): RssChannel = repository.getRssChannel()
 }
